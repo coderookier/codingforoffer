b8939db3d433d2094e199c94bad8c13888032799
coderookier <gupp0816@163.com>
2019-12-02 20:54:43 +0800

    Question50_2,3

---
+++ src/Chapter6/Question50_2.java
@@ -0,0 +1,32 @@
+package Chapter6;
+
+/**
+ * @author gutongxue
+ * @date 2019/12/2 20:13
+ * 删除字符串中所有重复出现的字符，如google-->gole
+ **/
+public class Question50_2 {
+    public String deleteRepeatingChar(String s) {
+        if (s == null || s.length() <= 0) {
+            return null;
+        }
+        String str = "";
+        //记录字符有没有出现，出现则赋值true
+        boolean[] flag = new boolean[256];
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!flag[ch]) {
+                str += ch;
+                flag[ch] = true;
+            }
+        }
+        return str;
+    }
+    public static void main(String[] args) {
+        Question50_2 question50_2 = new Question50_2();
+        String s = "google";
+        String s2 = "aaaaaaaaaaaaaa";
+        System.out.println(question50_2.deleteRepeatingChar(s));
+        System.out.println(question50_2.deleteRepeatingChar(s2));
+    }
+}
---
+++ src/Chapter6/Question50_3.java
@@ -0,0 +1,37 @@
+package Chapter6;
+
+/**
+ * @author gutongxue
+ * @date 2019/12/2 20:39
+ * 判断两个单词是否互为变位词，即字母出现次数相同
+ **/
+public class Question50_3 {
+    public boolean isAnagram(String s1, String s2) {
+        if (s1 == null || s1.length() <= 0 || s2 == null || s2.length() <= 0) {
+            return false;
+        }
+        int[] arr = new int[256];
+        //统计字符串1中各个字符出现的次数
+        for (int i = 0; i < s1.length(); i++) {
+            char ch = s1.charAt(i);
+            arr[ch] += 1;
+        }
+        //遍历字符串2，字符每出现一次对应位置元素值-1
+        for (int j = 0; j < s2.length(); j++) {
+            char ch = s2.charAt(j);
+            arr[ch] -= 1;
+        }
+        for (int num : arr) {
+            //数组中有元素值不为0则false
+            if (num != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Question50_3 question50_3 = new Question50_3();
+        System.out.println(question50_3.isAnagram("listen", "silent"));
+    }
+}
